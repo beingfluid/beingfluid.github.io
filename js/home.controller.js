f2fe9965088dbcec0f97857bf8eb466bd8b06c9f
Vishal Ingle <vishalingle25793@gmail.com>
2022-05-21 08:40:38 +0530

    Small spell correction

--- js/home.controller.js
+++ js/home.controller.js
@@ -5,5 +5,5 @@ HomeController.$inject = [];
 function HomeController(  ) {
     this.welcome_message = "Hello, I'm Vishal Ingle.";
     this.welcome_short_desc = "I'm a Developer & Business Analyst based in India.";
-    this.welcome_desc = "I love to explore and learn new things. Programming and writting is something I'm passionate about.";
+    this.welcome_desc = "I love to explore and learn new things. Programming and writing is something I'm passionate about.";
 }
